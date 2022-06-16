2d9e8d292fd98d85de8c2f51d280a153537fded0
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-06-16 06:35:16 -0400

    Update email-form.js

--- Newsletter-2/assets/email-form.js
+++ Newsletter-2/assets/email-form.js
@@ -3,7 +3,7 @@ document.getElementById("gform").addEventListener('submit', (e) => {
   // Timeout is needed for form to properly submit with animation
   setTimeout(function(){
    
-  var subscribeForm = document.getElementById("subscribeForm")
+  var subscribeForm = document.getElementById("emailFormCard")
  subscribeForm.setAttribute("style", "-webkit-animation: fadeOut 1s; animation: fadeOut 1s;  animation-fill-mode: forwards;");
  document.getElementById("gform").setAttribute("style", "display:none;");  
  
