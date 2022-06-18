178d1ef8a0bfa163e04d978046d45bef191c8b60
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-06-18 02:59:36 -0400

    Create replace-vars.js

---
+++ Newsletter-2/assets/replace-vars.js
@@ -0,0 +1,13 @@
+ // Replacing Variables At Top Of This Document
+  
+  var emailFormCard = document.getElementById("emailFormCard") ;
+emailFormCard.innerHTML = emailFormCard.innerHTML
+                        .replace(/Google_Entry_ID_Name/g, Google_Entry_ID_Name)
+                         .replace(/GOOGLE_FORM_URL/g, Google_Form_Link)
+  
+  .replace(/ Google_Entry_ID_Email/g,  Google_Entry_ID_Email)
+  
+ 
+  
+ 
+  ;
